03f701d54a637a0c12b33958c19a570f250e8fca
yuya-m-ms <maemichi@monosense.co.jp>
2016-07-21 15:20:15 +0900

    Rename resource

--- main.tf
+++ main.tf
@@ -10,18 +10,18 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-resource "aws_vpc" "My_VPC" {
+resource "aws_vpc" "main" {
   cidr_block           = "10.1.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "false"
   tags {
-    Name = "My_VPC"
+    Name = "terraform-main"
   }
 }
 
-resource "aws_internet_gateway" "My_GW" {
-  vpc_id = "${aws_vpc.My_VPC.id}"
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.main.id}"
 }
 
 variable "n-public" {
@@ -38,25 +38,25 @@ variable "n-c" {
 }
 
 resource "aws_subnet" "public-a" {
-  vpc_id            = "${aws_vpc.My_VPC.id}"
+  vpc_id            = "${aws_vpc.main.id}"
   cidr_block        = "10.1.${var.n-public + var.n-a}.0/20"
   availability_zone = "${var.region}a"
 }
 
 resource "aws_subnet" "public-c" {
-  vpc_id            = "${aws_vpc.My_VPC.id}"
+  vpc_id            = "${aws_vpc.main.id}"
   cidr_block        = "10.1.${var.n-public + var.n-c}.0/20"
   availability_zone = "${var.region}c"
 }
 
 resource "aws_subnet" "private-a" {
-  vpc_id            = "${aws_vpc.My_VPC.id}"
+  vpc_id            = "${aws_vpc.main.id}"
   cidr_block        = "10.1.${var.n-private + var.n-a}.0/20"
   availability_zone = "${var.region}a"
 }
 
 resource "aws_subnet" "private-c" {
-  vpc_id            = "${aws_vpc.My_VPC.id}"
+  vpc_id            = "${aws_vpc.main.id}"
   cidr_block        = "10.1.${var.n-private + var.n-c}.0/20"
   availability_zone = "${var.region}c"
 }
@@ -68,19 +68,19 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "ngw" {
   allocation_id = "${aws_eip.nat.id}"
   subnet_id     = "${aws_subnet.private-a.id}"
-  depends_on    = ["aws_internet_gateway.My_GW"]
+  depends_on    = ["aws_internet_gateway.igw"]
 }
 
 resource "aws_route_table" "public-route" {
-  vpc_id = "${aws_vpc.My_VPC.id}"
+  vpc_id = "${aws_vpc.main.id}"
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.My_GW.id}"
+    gateway_id = "${aws_internet_gateway.igw.id}"
   }
 }
 
 resource "aws_route_table" "private-route" {
-  vpc_id = "${aws_vpc.My_VPC.id}"
+  vpc_id = "${aws_vpc.main.id}"
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_nat_gateway.ngw.id}"
