fcec92aa46d7a6eb8cb183761ecc9cbd5eb4b5c5
yuya-m-ms <maemichi@monosense.co.jp>
2016-07-21 13:00:19 +0900

    Add resource of VPC, IGW

--- main.tf
+++ main.tf
@@ -9,3 +9,17 @@ provider "aws" {
     secret_key = "${var.aws_secret_key}"
     region     = "${var.region}"
 }
+
+resource "aws_vpc" "My_VPC" {
+    cidr_block           = "10.1.0.0/16"
+    instance_tenancy     = "default"
+    enable_dns_support   = "true"
+    enable_dns_hostnames = "false"
+    tags {
+        Name = "My_VPC"
+    }
+}
+
+resource "aws_internet_gateway" "My_GW" {
+    vpc_id = "${aws_vpc.My_VPC.id}"
+}
